be28da68ba9ae41f194b41e52e88354551385d14
Edison Calderon <edison.calderon@provedatos.com>
2019-07-08 10:28:13 -0500

    No mostar paginacion en reporte

--- app/employees/controllers/report.controller.js
+++ app/employees/controllers/report.controller.js
@@ -2,7 +2,7 @@ angular.module("App")
   .controller("report", function($scope, $route, $location, employees_model, NgTableParams) {
     employees_model.get('list')
     .then(function(res) {
-      	$scope.employeesTable = new NgTableParams({}, { dataset: res.data });
+      	$scope.employeesTable = new NgTableParams({}, { dataset: res.data, counts: [] });
       });
 
   });
